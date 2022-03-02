6102c9091ed77ce2e4b7214324393f42e526d04c
David Barrat <david@barrat.io>
2022-03-02 23:04:06 +0100

    adding dist to ignored patterns

--- packages/web-sdk/jest.config.js
+++ packages/web-sdk/jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
     },
   },
   moduleFileExtensions: ['ts', 'js'],
+  modulePathIgnorePatterns: ["<rootDir>/dist/"],
   transform: {
     '^.+\\.(ts)$': 'ts-jest',
   },
@@ -12,15 +13,5 @@ module.exports = {
   testPathIgnorePatterns: ['/node_modules/', 'build'],
   testEnvironment: 'node',
   rootDir: '.',
-  collectCoverageFrom: [
-    // "!<rootDir>/src/activity/*.ts",
-    // "<rootDir>/src/db/**/*.ts",
-    // "<rootDir>/src/jwks/**/*.ts",
-    // "<rootDir>/src/oauth2/**/*.ts",
-    // "<rootDir>/src/resolvers/**/*.ts",
-    // "<rootDir>/src/schema/**/*.ts",
-    // "<rootDir>/src/security/**/*.ts",
-    // "<rootDir>/src/utils/**/*.ts",
-    // "<rootDir>/src/vendors/**/*.ts"
-  ],
+  collectCoverageFrom: []
 };
