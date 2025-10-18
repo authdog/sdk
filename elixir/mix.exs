5f1be68c433797a4d3de46aaab2345e5ad4e09ec
David <david@barrat.io>
2025-10-18 17:58:54 +0200

    Add elixir tests

--- elixir/mix.exs
+++ elixir/mix.exs
@@ -34,7 +34,6 @@ defmodule Authdog.MixProject do
       {:ex_doc, "~> 0.30", only: :dev, runtime: false},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.3", only: [:dev], runtime: false},
-      {:ex_unit, "~> 1.12", only: :test},
       {:excoveralls, "~> 0.18", only: :test}
     ]
   end
