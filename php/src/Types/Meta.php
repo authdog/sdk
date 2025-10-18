d60b3757c74a763d4f99fb5aaf9c9d53dd64960a
David <david@barrat.io>
2025-10-18 17:54:39 +0200

    Scaffold php workflow

--- php/src/Types/Meta.php
+++ php/src/Types/Meta.php
@@ -9,18 +9,21 @@ class Meta
 {
     public int $code;
     public string $message;
+    public string $requestId;
 
     public function __construct(array $data)
     {
         $this->code = $data['code'] ?? 0;
         $this->message = $data['message'] ?? '';
+        $this->requestId = $data['requestId'] ?? '';
     }
 
     public function get(string $key): mixed
     {
         return match($key) {
-            'requestId' => $this->message,
+            'requestId' => $this->requestId,
             'code' => $this->code,
+            'message' => $this->message,
             default => null
         };
     }
