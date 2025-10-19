0015142d838bd3f8f4223a8ccc55c535335acc99
David <david@barrat.io>
2025-10-19 14:02:47 +0200

    Fix ruby tests

--- ruby/spec/authdog_client_spec.rb
+++ ruby/spec/authdog_client_spec.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'spec_helper'
 require_relative "../lib/authdog"
 
 RSpec.describe Authdog::Client do
--- ruby/spec/spec_helper.rb
+++ ruby/spec/spec_helper.rb
@@ -3,6 +3,8 @@
 require "rspec"
 require "webmock/rspec"
 
+WebMock.disable_net_connect!(allow_localhost: true)
+
 RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
